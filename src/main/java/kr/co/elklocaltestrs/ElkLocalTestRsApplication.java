30b25d72560232cc6011734ee39591f56529c0dd
janghomoon <sonyjangho@gmail.com>
2025-09-06 22:53:00 +0900

    Initial commit

---
+++ src/main/java/kr/co/elklocaltestrs/ElkLocalTestRsApplication.java
@@ -0,0 +1,13 @@
+package kr.co.elklocaltestrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElkLocalTestRsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElkLocalTestRsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/elklocaltestrs/ElkLocalTestRsApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.elklocaltestrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElkLocalTestRsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
